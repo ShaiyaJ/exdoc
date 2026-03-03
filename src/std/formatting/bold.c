5e97efad5ad766092a5b6b56485e0d98ca35e8d2
slnishi <93013772+ShaiyaJ@users.noreply.github.com>
2026-03-03 23:41:01 +0000

    bold file checking working

--- src/std/formatting/bold.c
+++ src/std/formatting/bold.c
@@ -4,19 +4,21 @@
 
 int main(void) {
     // Getting file extension 
-    char* ext;
-    size_t ext_len;
+    char* ext = NULL;
+    size_t len;
+    int ext_len = getline(&ext, &len, stdin);
+    int no_nl = ext_len - 1;                    // Size without newline
 
-    if (getline(&ext, &ext_len, stdin) == -1)   // Error code
+    if (ext_len == -1)                          // Error checking
         return 1;
 
     // Getting the rest of the text
-    char* content;
+    char* content = "hello";
     
     // Printing out bold character based on ext
-    if      (strncmp(ext, ".txt",  len)) printf("**%s**", content);
-    else if (strncmp(ext, ".md",   len)) printf("**%s**", content);
-    else if (strncmp(ext, ".html", len)) printf("<strong>%s</strong>", content);
+    if      (strncmp(ext, ".txt",  no_nl) == 0) printf("**%s**", content);
+    else if (strncmp(ext, ".md",   no_nl) == 0) printf("**%s**", content);
+    else if (strncmp(ext, ".html", no_nl) == 0) printf("<strong>%s</strong>", content);
 
     // Cleanup
     free(ext);
