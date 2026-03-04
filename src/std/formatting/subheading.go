2912bdd3cb2573504416a910faa6dfef747e0e35
slnishi <93013772+ShaiyaJ@users.noreply.github.com>
2026-03-04 22:40:15 +0000

    switched standard library to go, untested

---
+++ src/std/formatting/subheading.go
@@ -0,0 +1,20 @@
+package main 
+
+import "fmt"
+
+func main() {
+    // Getting extension
+    var ext string
+    fmt.Scanln(&ext) // TODO: error handling:
+
+    // Getting content
+    var content string
+    fmt.Scanf("%s", &content) // TODO: error handling
+
+    // Outputting value based on ext
+    switch ext {
+        case ".md":		fmt.Print("## ", content)
+        case ".html":	fmt.Print("<h2>", content, "</h2>")
+        default: 		fmt.Print("----- ", content, " -----") 
+	}
+}
